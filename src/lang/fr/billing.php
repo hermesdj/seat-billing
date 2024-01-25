d1e93465e4e39ff777c41932b98b44809dd9553e
Gelope <157318045+Gelope@users.noreply.github.com>
2024-01-25 20:24:04 +0100

    Terminé

---
+++ src/lang/fr/billing.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    // General
+    'settings' => 'Paramètres',
+    'summary' => 'Récapitulatif de facturation',
+    'summary-live' => 'Récapitulatif de facturation actuel',
+    'summary-corp-mining' => 'Corporation Minière',
+    'summary-corp-pve' => 'Corporation PvE',
+    'summary-ind-mining' => 'Exploitation minière individuelle',
+    'pastbill' => 'Factures antérieures',
+    'previousbill' => 'Factures précédentes',
+    'user_bills' => 'Factures des utilisateurs',
+    'info' => 'Info',
+    'no_user_tax_history' => 'Il semble qu\'aucun historique de taxe minière ne soit disponible pour vous..',
+    'character' => 'Personnage',
+    'corporation' => 'Corporation',
+    'mining_tax' => 'Taxe minière',
+    'mining_amount' => 'Montant minier',
+    'isk' => 'ISK',
+    'total' => 'Total',
+    'tax_invoice_message' => 'taxe minières de facturation-seat pour :month/:year',
+    'tax_invoices' => 'Factures fiscales',
+
+    // New Translation keys
+    'permission_view_billing' => 'Afficher la facturation',
+    'permission_view_billing_desc' => 'Vous permet d\'afficher les données de facturation',
+    'permission_configure_billing' => 'Configurer la facturation',
+    'permission_configure_billing_desc' => 'Vous permet de modifier les données de facturation',
+    'permission_manage_tax_invoices' => 'Gérer les factures fiscales',
+    'permission_manage_tax_invoices_desc' => 'Vous permet de visualiser et de gérer les factures fiscales.',
+
+    'billing_menu_label' => 'Facturation',
+    'billing_data_menu_label' => 'Données de facturation',
+    'billing_settings_menu_label' => 'Paramètres',
+    'billing_personal_menu_label' => 'utilisateurs',
+    'billing_tax_invoices_menu_label' => 'Factures fiscales',
+    'billing_corp_tax_menu_label' => 'Gestion fiscale',
+
+];
